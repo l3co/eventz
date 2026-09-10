01699dc5608c24b16e2cfee3e04d580309a502bc
l3co <lcosta.sp.br@gmail.com>
2026-09-10 12:28:36 -0300

    feat: implement current_user helper and require_login before action for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
   resources :users
+  get "signin", to: "sessions#new"
   get "signup", to: "users#new"
+  delete "signout", to: "sessions#destroy"
 
 end
