457577362c81a6947789f3f7df2cebde5ab2beca
l3co <lcosta.sp.br@gmail.com>
2026-09-14 11:45:39 -0300

    feat: add likes functionality with associations to users and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,12 @@
 class Event < ApplicationRecord
 
   has_many :registrations, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :name, :location, presence: true
-
   validates :description, length: { minimum: 25 }
-
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
-
   validates :image_file_name, format: { with: /\w+\.(gif|jpg|png)\z/i, message: "must be a GIF, JPG, or PNG image" }
 
   def free? = price.blank? || price.zero?
