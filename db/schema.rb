c46c2c0eb04c575b1b5e23f8ffce10f0fb00bde0
l3co <lcosta.sp.br@gmail.com>
2026-09-14 10:35:45 -0300

    feat: refactor registrations table to establish user associations and remove unnecessary fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_12_002342) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_14_132454) do
   create_table "events", force: :cascade do |t|
     t.integer "capacity", default: 1
     t.datetime "created_at", null: false
@@ -25,11 +25,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_12_002342) do
 
   create_table "registrations", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "email"
     t.integer "event_id", null: false
     t.string "how_heard"
-    t.string "name"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["event_id"], name: "index_registrations_on_event_id"
   end
 
